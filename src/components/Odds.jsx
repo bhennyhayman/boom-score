85c61ad1889a267613773312e7f408104e70be50
Bhenny Hayman <bengero686@gmail.com>
2025-08-19 10:06:50 +0000

    new components

---
+++ src/components/Odds.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Odds = () => {
+  return (
+    <div>Odds</div>
+  )
+}
+
+export default Odds
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import useAuth from '../context/useAuth'
 import { MdClose } from 'react-icons/md'
+import {NavLink} from 'react-router-dom'
 
 const SideBar = () => {
   const {setOpenBar} = useAuth();
 
   return (
-    <div className='fixed top-0 h-screen bg-teal-500 w-70'>
+    <div className='fixed top-0 h-screen bg-teal-500 w-70 text-white'>
       
-      <div className='text-white flex justify-between items-center-last mt-5'>
-        <div className='ml-5 text-2xl'>BoomScore</div>
+      <div className=' flex justify-between items-center-last mt-5'>
+        <div className='ml-5 text-2xl '>BoomScore</div>
         <div className='mr-9 cursor-pointer bg-white px-2 py-2 text-black rounded-full' onClick={()=>setOpenBar(false)}><MdClose size={20}/></div>
       </div>
+      <hr className='mt-5 text-teal-600'/>
+
+      <div className="sideLinks text-[18px] py-3">
+        <div className='sideLink'><NavLink to={'/'}>Home</NavLink></div>
+        <div className='sideLink'><NavLink to={'/matches'}>Today Matches</NavLink></div>
+        <div className='sideLink'><NavLink to={'/predictions'}>Betting Odds</NavLink></div>
+        <div className='sideLink'><NavLink to={'/contact'}>Contact</NavLink></div>
+        <div className='sideLink'><NavLink to={'/about'}>About </NavLink></div>
+        <div className='sideLink'><NavLink to={'/matches'}>Faqs</NavLink></div>
+      </div>
+      
       
     </div>
   )
